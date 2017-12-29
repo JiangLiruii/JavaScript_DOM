f6c246be83099ef7d683d637aba755de75251fe8
Big_Panda <lorry.jiang@hotmail.com>
2017-12-29 22:33:36 +0800

    修改AccessKey函数，使其检索到citations中的a标签时不会返回false导致函数不执行

--- Chapter8_Fullfill_content/displayAccessKey.js
+++ Chapter8_Fullfill_content/displayAccessKey.js
@@ -9,21 +9,22 @@ function displayAccessKey() {
   // }
 // }
   const links = document.getElementsByTagName('a');
+  // console.log(links);
   const header = document.createElement('h2');
   const headerText = document.createTextNode('AccessKeys');
   const list = document.createElement('ul');
   for (let i = 0; i < links.length; i++) {
     if (links[i].hasAttribute('accessKey')) {
       const accessKey = links[i].getAttribute('accesskey');
-      console.log(accessKey);
+      // console.log(accessKey);
       const str = `${links[i].text}:${accessKey}`;
-      console.log(str);
+      // console.log(str);
       const item = document.createElement('li');
       const text = document.createTextNode(str);
       item.appendChild(text);
       list.appendChild(item);
-      console.log(item);
-    } else return false;
+      // console.log(item);
+    } else console.log(`${links[i]} dont have accesskey attribute`);
   }
   header.appendChild(headerText);
   document.body.appendChild(header);
