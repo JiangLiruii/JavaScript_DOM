54b19f907fa4330b62b291d417969db21f1d1340
Big_Panda <lorry.jiang@hotmail.com>
2017-12-29 09:38:20 +0800

    添加小更改

--- Chapter8_Fullfill_content/displayAccessKey.js
+++ Chapter8_Fullfill_content/displayAccessKey.js
@@ -1,10 +1,18 @@
 function displayAccessKey() {
-  const keys = document.getElementById('navigation');
-  const liChild = keys.childNodes;
-  console.log(liChild);
+//   const keys = document.getElementById('navigation');
+//   const liChild = keys.childNodes;
+//   console.log(liChild);
 //   if (!(liChild.length === 0)) {
 //     for (let i = 0; i < liChild.length; i++) {
 //       const accesskey = liChild[i];
 //     }
   // }
+// }
+  const links = document.getElementsByTagName('a');
+  const header = document.createElement('h2');
+  for (let i = 0; i < links.length; i++) {
+    const accessKey = links[i].accesskey;
+    const accessKeyText = document.createTextNode('AccessKeys');
+    accessKeyText.appendChild(accessKey)
+  }
 }
