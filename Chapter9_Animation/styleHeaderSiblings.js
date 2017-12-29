d94d0b2693c0d80606d0c13f7a93cc1df8da340e
Big_Panda <lorry.jiang@hotmail.com>
2017-12-29 22:38:17 +0800

    通过检索元素实现批量添加或修改样式

---
+++ Chapter9_Animation/styleHeaderSiblings.js
@@ -0,0 +1,16 @@
+function getElementSibling(node) {
+  if (node.nodeType === 1) return node;
+  if (node.nextSibling) return getElementSibling(node.nextSibling);
+  return null;
+}
+function styleHeaderSiblings() {
+  if (!document.getElementsByTagName) return false;
+  const headers = document.getElementsByTagName('h1');
+  for (let i = 0; i < headers.length; i++) {
+    const elem = getElementSibling(headers[i].nextSibling);
+    // console.log(elem,headers[i].nextSibling);
+    elem.style.fontWeight = 'bold';
+    elem.style.fontSize = '1.2em';
+  }
+}
+addOnload(styleHeaderSiblings);
